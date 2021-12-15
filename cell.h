543155db2e189586a28042a2eb86ee790a3664a0
Jafarichen <3149829972@qq.com>
2021-12-15 17:09:37 +0800

    创建Cell

---
+++ cell.h
@@ -0,0 +1,14 @@
+#ifndef CELL_H
+#define CELL_H
+
+
+class Cell
+{
+public:
+    int weight;//对应边的权值
+    bool coverd;//0表示未覆盖，1表示覆盖
+    bool validity;//0表示边不存在，1表示存在
+    Cell();
+};
+
+#endif // CELL_H
